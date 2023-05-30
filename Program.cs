f58ea91a432f1d7c44fe2c099e6f422d6b81a441
David Duron <86013234+emt2dev@users.noreply.github.com>
2023-05-29 21:44:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", 
+        adding => adding.AllowAnyHeader()
+        .AllowAnyOrigin()
+        .AllowAnyMethod());
+});
+
+// Add Serilog
+builder.Host.UseSerilog((builderContext, loggerConfig) => 
+    loggerConfig.WriteTo.Console()
+    .ReadFrom
+    .Configuration(builderContext.Configuration));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // Swagger Documentation/Debugging
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
